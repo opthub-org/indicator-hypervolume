b3b7b8b48b5b358ee28b0b07768a47782ddddd9b
Naoki Hamada <hmkz@s101.xrea.com>
2020-06-14 07:57:16 +0900

    Create hy.py

---
+++ hy.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+"""
+Hypervolume indicator.
+"""
+import json
+import os
+import sys
+
+from pygmo import hypervolume
+
+
+def main():
+    solution_to_score = json.loads(sys.argv[1])
+    solutions_scored = json.loads(sys.argv[2])
+
+    ys = [s['objective'] for s in solutions_scored]
+    ys.append(solution_to_score['objective'])
+    hv = hypervolume(ys)
+    ref_point = json.loads(os.getenv('HV_REF_POINT', 'null'))
+    score = hv.compute(ref_point)
+    print(score)
+
+
+if __name__ == '__main__':
+    main()
